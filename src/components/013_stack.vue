6f1514aacd344d82252513350e2b84160fa3d487
jiawei.tang <billl89@126.com>
2022-10-27 06:35:30 +0800

    stack

---
+++ src/components/013_stack.vue
@@ -0,0 +1,5 @@
+<template>
+  <div style="padding-left: 20px;">
+    <slot></slot>
+  </div>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import FormTable from './pages/formTable.vue';
 import FormTableWithUse from './pages/formTableWithUse.vue';
 import GenForm from './pages/genForm.vue';
 import GenFormRef from './pages/genFormRef.vue';
+import Test_stack from './pages/013_test_stack.vue';
 
 const routes = [
   { path: '/app', component: App },
@@ -12,6 +13,7 @@ const routes = [
   { path: '/formTableWithUse', component: FormTableWithUse },
   { path: '/genForm', component: GenForm },
   { path: '/genFormRef', component: GenFormRef },
+  { path: '/013_test_stack', component: Test_stack },
 ]
 
 const router = createRouter({
