db407810e2a5c642ac171e25ffbf171575c8d802
jiawei.tang <billl89@126.com>
2023-06-16 09:03:06 +0800

    css var

--- src/pages/015_css_var.vue
+++ src/pages/015_css_var.vue
@@ -1,12 +1,22 @@
 <template>
-  <Stack>
-    <div>a</div>
-    <Stack>
-      <div>aa</div>
-    </Stack>
-  </Stack>
+  <div>123</div>
 </template>
 
 <script lang="ts" setup>
-import Stack from '../components/013_stack.vue';
+const root = document.documentElement;
+root.style.setProperty('--blue', '#3f1a6a');
+root.style.setProperty('--second-color', 'blue');
 </script>
+
+<style scoped>
+
+  :root {
+    --blue: #9fea26;
+  }
+
+  div{
+    --blue: #1e90ff;
+
+    color: var(--blue);
+  }
+</style>
