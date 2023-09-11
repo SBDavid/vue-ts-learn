59e3141cacf9922ba3a3603cce87e60136efd74b
jiawei.tang <jiawei.tang@nio.com>
2023-09-11 10:07:34 +0800

    021_travel_vue_tree : 查找孩子

--- src/pages/021_travel_vue_tree.vue
+++ src/pages/021_travel_vue_tree.vue
@@ -1,17 +1,28 @@
 <template>
   <Child name="c1"></Child>
-  <Child name="c2"></Child>
+  <Child name="c2">
+    <div>test slot</div>
+  </Child>
   <div>c3</div>
 </template>
-<script>
-import { defineComponent, onMounted, getCurrentInstance } from 'vue'
+<script lang="ts">
+import { defineComponent, onMounted, getCurrentInstance, Fragment, type VNode,  } from 'vue'
 import Child from './021_travel_vue_tree_child.vue'
 export default defineComponent({
   name: '021_travel_vue_tree',
   setup: (_, { expose }) => {
     onMounted(() => {
+      console.info('mounted 021_travel_vue_tree')
       const ins = getCurrentInstance()
       console.info(ins)
+
+      // 查找第一个孩子
+      if (ins?.subTree.children) {
+        const comp = (ins.subTree.children as Array<VNode>)[0].component
+        
+        console.info(comp?.exposed)
+        comp?.exposed?.add()
+      }
     })
     expose({
       'ExposeFrom021_travel_vue_tree': 1
--- src/pages/021_travel_vue_tree_child.vue
+++ src/pages/021_travel_vue_tree_child.vue
@@ -1,18 +1,33 @@
 <template>
   <p>child: {{ name }}</p>
+  <p>count: {{ count }}</p>
+  <slot></slot>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
 export default defineComponent({
   name: '021_travel_vue_tree_child',
   props: {
     name: String
   },
   setup: (_, { expose }) => {
+    const count = ref(0)
+    const add = () => {
+        count.value++
+      }
     expose({
-      'ExposeFrom021_travel_vue_tree_child': 11
+      'ExposeFrom021_travel_vue_tree_child': 11,
+      add,
     })
-    _.name
+
+    onMounted(() => {
+      console.info('mounted 021_travel_vue_tree_child')
+      add()
+    })
+
+    return {
+      count
+    }
   }
 })
 
